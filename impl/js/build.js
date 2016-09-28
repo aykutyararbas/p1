156eb9e438f8a438c01ad891d8beaebc12e548ee
Aykut Yararbas <aykut_yararbas@bmc.com>
2016-09-27 21:57:08 -0700

    p1.min

--- impl/js/build.js
+++ impl/js/build.js
@@ -3,7 +3,7 @@ var compressor = require('node-minify');
 new compressor.minify({
   type: 'gcc',
   fileIn: ['src/p1.js'],
-  fileOut: 'dist/p1.js',
+  fileOut: 'dist/p1.min.js',
   callback: function(err, min){
     console.log(err);
     //console.log(min); 
